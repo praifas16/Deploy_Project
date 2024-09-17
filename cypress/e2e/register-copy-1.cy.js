39b95ca9d8f1adda0dfb3323cc5ae0182786003e
prayfa sonchai <prayfasonchai@MacBook-Air-khxng-prayfa.local>
2024-09-17 15:07:38 +0700

    first commit

---
+++ cypress/e2e/register-copy-1.cy.js
@@ -0,0 +1,6 @@
+describe('Test Register Page', () => {
+  it('should load the register page', () => {
+    cy.visit('http://127.0.0.1:5500/register.html')
+    cy.contains('Register') // ตรวจสอบว่ามีข้อความ 'Register' บนหน้าเว็บ
+  })
+})
